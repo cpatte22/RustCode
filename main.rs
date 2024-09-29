9e8a7529c51f96978e83158bb41c1b3bc8adb42e
cpatte22 <90731051+cpatte22@users.noreply.github.com>
2024-09-28 21:51:31 -0700

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,6 +1,8 @@
+use std::io;
 fn main(){
-    //let mut num = 11;
-    let num: i8 = 11;
+    //Test the basic
+    /*
+    let mut num: i8 = 11;
     let num3: i8 = 13;
 
     let j = num + num3;
@@ -9,4 +11,23 @@ fn main(){
     println!("value of num: {}", num);
     //num = 13;
     println!("value of num: {}", num);
+    */
+
+    //Testing basic I/O
+    println!("Enter two numbers to add\nnum_1: ");
+    let mut user_in_num1 = String::new(); //string variable for holding user input
+    let mut user_in_num2 = String::new();
+
+    io::stdin().read_line(&mut user_in_num1).expect("Failed to read input"); //storing user input in variable
+    println!("num_2: ");
+    io::stdin().read_line(&mut user_in_num2).expect("Failed to read second input");
+
+    let user_in_num1_int: i32 = user_in_num1.trim().parse().unwrap(); //convert user string to int
+    let user_in_num2_int: i32 = user_in_num2.trim().parse().unwrap();
+    
+    let sum = user_in_num1_int + user_in_num2_int; //perform addition calculation
+    println!("The sum is {}", sum);
+
+
+    
 }
