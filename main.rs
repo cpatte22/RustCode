1434f012ebff19e06dceb0e963fdfdbbc12d63f4
cpatte22 <90731051+cpatte22@users.noreply.github.com>
2024-09-28 21:28:16 -0700

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,12 @@
+fn main(){
+    //let mut num = 11;
+    let num: i8 = 11;
+    let num3: i8 = 13;
+
+    let j = num + num3;
+    println!("{}", j);
+    println!("this is rust");
+    println!("value of num: {}", num);
+    //num = 13;
+    println!("value of num: {}", num);
+}
