82fbd062ab4019284855dd638283f51ecf44b3f8
cpatte22 <90731051+cpatte22@users.noreply.github.com>
2024-10-08 20:42:21 -0700

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+int main() {
+   printf("This is the C program");
+   return 0;
+}
+*/
+void cFoo(){
+
+   //Example of I/O in C
+   int num1, num2;
+   scanf("Enter num1 (C): %d", num1);
+   scanf("Enter num2 (C): %d", num2);
+
+   int sum = num1 + num2;
+   printf("The sume of the numbers is %d", sum);
+   printf("This is the C Program");
+}
--- main.rs
+++ main.rs
@@ -9,14 +9,14 @@ fn main() {
     println!("Calling C code from Rust...");
     
     //Testing basic I/O
-    println!("Enter two numbers to add\nnum_1: ");
+    println!("Enter two numbers to add\nnum_1 (Rust): ");
     let mut user_in_num1 = String::new(); //string variable for holding user input
     let mut user_in_num2 = String::new();
 
     
 
     io::stdin().read_line(&mut user_in_num1).expect("Failed to read input"); //storing user input in variable
-    println!("num_2: ");
+    println!("num2 (Rust): ");
     io::stdin().read_line(&mut user_in_num2).expect("Failed to read second input");
 
     let user_in_num1_int: i32 = user_in_num1.trim().parse().unwrap(); //convert user string to int
