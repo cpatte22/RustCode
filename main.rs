c37f0afbc534653f52b6dc22275073a93b3cf166
cpatte22 <90731051+cpatte22@users.noreply.github.com>
2024-10-08 20:36:29 -0700

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,32 @@
+use std::io;
+
+
+extern "C" {
+    fn cFoo();
+}
+
+fn main() {
+    println!("Calling C code from Rust...");
+    
+    //Testing basic I/O
+    println!("Enter two numbers to add\nnum_1: ");
+    let mut user_in_num1 = String::new(); //string variable for holding user input
+    let mut user_in_num2 = String::new();
+
+    
+
+    io::stdin().read_line(&mut user_in_num1).expect("Failed to read input"); //storing user input in variable
+    println!("num_2: ");
+    io::stdin().read_line(&mut user_in_num2).expect("Failed to read second input");
+
+    let user_in_num1_int: i32 = user_in_num1.trim().parse().unwrap(); //convert user string to int
+    let user_in_num2_int: i32 = user_in_num2.trim().parse().unwrap();
+    
+    let sum = user_in_num1_int + user_in_num2_int; //perform addition calculation
+    println!("The sum is {}", sum);
+
+    //call the C function
+    unsafe {
+        cFoo();
+    }
+}
